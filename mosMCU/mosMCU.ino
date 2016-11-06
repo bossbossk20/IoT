0b3b53771f64cc3068917c0aad9846b54f884fd3
thanawat <tha_chookes@hotmail.com>
2016-11-06 11:26:16 +0700

    button

--- mosMCU/mosMCU.ino
+++ mosMCU/mosMCU.ino
@@ -1,11 +1,3 @@
-//void setup() {
-//  pinMode(D2, INPUT);
-//}
-//
-//void loop() {
-//  Serial.print(digitalRead(D2));
-//}
-
 const int buttonPin = D2;    
 int buttonState = 0;      
 
@@ -17,9 +9,6 @@ void setup() {
 void loop() {
 
   buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
     // turn LED on:
     Serial.println("on");
