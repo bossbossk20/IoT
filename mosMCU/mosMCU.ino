ce26ce8439c42eaae23cecd8be963fc2c41c48a8
thanawat <tha_chookes@hotmail.com>
2016-12-02 17:20:07 +0700

    updated RGB LED 5mm :tada:

--- mosMCU/mosMCU.ino
+++ mosMCU/mosMCU.ino
@@ -2,8 +2,8 @@
 const int  buttonPin = D2;
 const int ledPin = D1;
 int buttonPushCounter = 0;
-int buttonState = 0;       
-int lastButtonState = 0; 
+int buttonState = 0;
+int lastButtonState = 0;
 const int redPin = D5;
 const int greenPin = D6;
 const int bluePin = D7;
@@ -16,15 +16,15 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);  
+  pinMode(bluePin, OUTPUT);
   Serial.begin(115200);
-  Serial.print("Connecting to "+*MY_SSID);
+  Serial.print("Connecting to " + *MY_SSID);
   WiFi.begin(MY_SSID, MY_PWD);
   Serial.println("going into wl connect");
 
-  while (WiFi.status() != WL_CONNECTED){
-      delay(1000);
-      Serial.print(".");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
   }
   Serial.println("wl connected");
   Serial.println("");
@@ -32,16 +32,10 @@ void setup() {
   Serial.println("");
 }
 
-void setColor(int red, int green, int blue)
-{
-  #ifdef COMMON_ANODE
-    red = 255 - red;
-    green = 255 - green;
-    blue = 255 - blue;
-  #endif
-  analogWrite(redPin, red);
-  analogWrite(greenPin, green);
-  analogWrite(bluePin, blue);  
+void RGBcolor(int red, int green, int blue) {
+  digitalWrite(redPin, !red);
+  digitalWrite(bluePin, !green);
+  digitalWrite(greenPin, !blue);
 }
 
 void loop() {
@@ -50,21 +44,22 @@ void loop() {
   if (buttonState != lastButtonState) {
     if (buttonState == HIGH) {
       Serial.println("pressed");
-        setColor(255, 0, 0);  // red
-        delay(1000);
-        setColor(0, 255, 0);
-        delay(1000);
-        setColor(0, 0, 255);
-      if (client.connect(WEBSITE, 80)){ 
-      client.print("GET /api?location=1");
-      client.println(" HTTP/1.1"); 
-      client.print("Host: ");
-      client.println(WEBSITE);
-      client.println("User-Agent: ESP8266/1.0");
-      client.println("Connection: close");
-      client.println();
-     }
-      
+      RGBcolor(1, 0, 0);
+      delay(1000);
+      RGBcolor(0, 1, 0);
+      delay(1000);
+      RGBcolor(0, 0, 1);
+      delay(1000);
+      if (client.connect(WEBSITE, 80)) {
+        client.print("GET /api?location=1");
+        client.println(" HTTP/1.1");
+        client.print("Host: ");
+        client.println(WEBSITE);
+        client.println("User-Agent: ESP8266/1.0");
+        client.println("Connection: close");
+        client.println();
+      }
+
     } else {
       Serial.println("haven't pressed yet");
     }
