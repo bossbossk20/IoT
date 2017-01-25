cdf8a873d5e0d0c8ac2408db0f722756fddb4994
thanawat <tha_chookes@hotmail.com>
2017-01-25 11:40:38 +0700

    updated LCD code

--- mcuTempInLCD/mcuTempInLCD.ino
+++ mcuTempInLCD/mcuTempInLCD.ino
@@ -1,26 +1,12 @@
 #include <LiquidCrystal.h>
-#include <DHT.h>
-#define DHTPIN D5
-DHT dht(DHTPIN, DHT22, 15);
 LiquidCrystal lcd(12, 14, 2, 0, 4, 5);
 void setup() {
   Serial.begin(115200);
-  dht.begin();
   lcd.begin(16, 2);
   lcd.setCursor(2, 0);
+//  lcd.print("test : ");
 }
 void loop() {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-  Serial.println(t);
-  lcd.setCursor(2, 1);
-  lcd.print("Temp : ");
-  lcd.print(t);
   lcd.setCursor(2, 0);
-  lcd.print("Hu : ");
-  lcd.print(h);
+  lcd.print("koykoy");
 }
